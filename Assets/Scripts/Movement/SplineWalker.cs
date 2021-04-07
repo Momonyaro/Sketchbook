3d2ac660b6ac068704ff8bc90a644583152c6c54
Momonyaro <sebastian.alkstrand@gmail.com>
2021-04-07 21:01:16 +0200

    You can now move along the spine slightly
    no jumping yet though, you don't deserve it just yet.

--- Assets/Scripts/Movement/SplineWalker.cs
+++ Assets/Scripts/Movement/SplineWalker.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Config;
 using UnityEngine;
 using PathCreation;
+using UnityEngine.InputSystem;
 
 namespace Movement
 {
@@ -14,6 +15,8 @@ namespace Movement
     {
         public bool assignSplineAtAwake = true;
         public PathCreator currentSpline;
+
+        private Vector2 lastDelta = Vector2.zero;
 
         private void Awake()
         {
@@ -28,6 +31,14 @@ namespace Movement
             AnchorXZToSpline();
         }
 
+        private void Update()
+        {
+            //Oh god, oh fuck this is terrible but the new input system made me do it...
+            if (Mathf.Abs(lastDelta.x) > 0.001f || Mathf.Abs(lastDelta.y) > 0.001f)
+                MoveAlongSplineHor(lastDelta.x);
+        }
+
+
         //Detta teleporterar objektet till den närmsta positionen längst med splinen.
         private void AnchorXZToSpline()
         {
@@ -38,6 +49,29 @@ namespace Movement
             if (!MapSettings.Instance.configScriptable.lockYToSpline) splinePos.y = position.y;
             
             transform.position = splinePos;
+        }
+
+        //Här tar vi den (horizontella riktningen * hastigheten) och flyttar spelaren längst splinen beroende på det.
+        public void MoveAlongSplineHor(float horSpeed)
+        {
+            //Varför ska vi röra oss?
+            //if (horSpeed == 0) return;
+            
+            var position = transform.position;
+            float splineDist = currentSpline.path.GetClosestDistanceAlongPath(position);
+            Vector3 splinePos = currentSpline.path.GetPointAtDistance(splineDist + horSpeed, EndOfPathInstruction.Stop);
+            
+            //This is garbage but ok for testing
+            if (!MapSettings.Instance.configScriptable.lockYToSpline) splinePos.y = position.y;
+            
+            transform.position = splinePos;
+        }
+
+        //This is working kinda weird, you can't hold the button down?
+        public void DebugTestMoveHor(InputAction.CallbackContext action)
+        {
+            Vector2 delta = action.ReadValue<Vector2>();
+            lastDelta = delta;
         }
     }
 }
