22370e23ae0f19f3f44f351481e0b1f54fa19ab2
Albin <a19albda@student.his.se>
2021-04-12 15:54:27 +0200

    Basic flashlight aiming, should have taken me five minutes but it didn't

---
+++ Assets/Scripts/Flashlight/FlashlightAim.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlashlightAim : MonoBehaviour
+{
+    bool aimingUp;
+    float yRotation = 0.0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.parent != null)
+        {
+            yRotation = transform.parent.eulerAngles.y;
+            print(yRotation);
+        }
+
+        if (aimingUp)
+            transform.rotation = Quaternion.Euler(new Vector3(0.0f, yRotation, -45.0f));
+        else
+            transform.rotation = Quaternion.Euler(new Vector3(0.0f, yRotation, -90.0f));
+    }
+
+    public void AimUp(InputAction.CallbackContext context)
+    {
+        aimingUp = context.ReadValueAsButton();
+    }
+}
