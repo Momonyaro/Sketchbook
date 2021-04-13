0404e287c958163e9ddadc03c929423e434311b5
Albin <a19albda@student.his.se>
2021-04-13 15:23:08 +0200

    2nd dev room (for programmers?), you can also now change if the flashlight is facing left or right along with being able to turn a """light""" """cone""" on and off, sorry for taking the whole day but I deadass don't understand this new input system

--- Assets/Scripts/Flashlight/FlashlightAim.cs
+++ Assets/Scripts/Flashlight/FlashlightAim.cs
@@ -5,26 +5,81 @@ using UnityEngine.InputSystem;
 
 public class FlashlightAim : MonoBehaviour
 {
+    [Tooltip("Default rotation of the flashlight (in degrees)")]
+    public float aimForwardRotation = 90.0f;
+    [Tooltip("Rotation of the flashlight while the player is aiming up (in degrees)")]
+    public float aimUpRotation = 45.0f;
+
+    [Tooltip("TRUE: Flashlight will toggle between being turned on or off when the flashlight button is pressed\n" +
+        "FALSE: Flashlight will only be turned on while the button is being held")]
+    public bool toggleLight = false;
+    bool previousFlashState = false;
+
     bool aimingUp;
+    bool flashButton, flashing;
+    float horDirection = -1.0f;
     float yRotation = 0.0f;
-    
-    // Update is called once per frame
+
+    // Bara för testing
+    [Tooltip("JUST FOR TESTING")]
+    public GameObject lightCone = null;
+
+    // Welcome to if statement city
     void Update()
     {
         if (transform.parent != null)
-        {
             yRotation = transform.parent.eulerAngles.y;
-            print(yRotation);
-        }
 
-        if (aimingUp)
-            transform.rotation = Quaternion.Euler(new Vector3(0.0f, yRotation, -45.0f));
+        if (!aimingUp)
+            transform.rotation = Quaternion.Euler(new Vector3(0.0f, yRotation, aimForwardRotation * horDirection));
         else
-            transform.rotation = Quaternion.Euler(new Vector3(0.0f, yRotation, -90.0f));
+            transform.rotation = Quaternion.Euler(new Vector3(0.0f, yRotation, aimUpRotation * horDirection));
+
+
+        // Detta får mig att vilja dö, finns det ingen variant av Input.GetButtonDown för ReadValueAsButton?
+        if (toggleLight)
+        {
+            if (flashButton && flashButton != previousFlashState)
+            {
+                flashing = !flashing;
+            }
+            previousFlashState = flashButton;
+        }
+        else
+            flashing = flashButton;
+
+        // Bara för testing part 2
+        if (lightCone != null)
+        {
+            if (flashing)
+                lightCone.SetActive(true);
+            else
+                lightCone.SetActive(false);
+        }
     }
 
-    public void AimUp(InputAction.CallbackContext context)
+    #region InputCalls
+
+    public void InputActionFlashing(InputAction.CallbackContext action)
     {
-        aimingUp = context.ReadValueAsButton();
+        flashButton = action.ReadValueAsButton();
     }
+
+    public void InputActionAimUp(InputAction.CallbackContext action)
+    {
+        if (action.ReadValue<Vector2>().y > 0.001f)
+            aimingUp = true;
+        else
+            aimingUp = false;
+    }
+
+    public void InputActionChangeHorDirection(InputAction.CallbackContext action)
+    {
+        if (action.ReadValue<Vector2>().x > 0.001f)
+            horDirection = 1.0f;
+        else if (action.ReadValue<Vector2>().x < -0.001f)
+            horDirection = -1.0f;
+    }
+
+    #endregion
 }
