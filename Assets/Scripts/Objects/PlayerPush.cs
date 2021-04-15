ebe6c64d5ea1d4c9da5c14072daddae853215176
Albin <a19albda@student.his.se>
2021-04-15 12:30:34 +0200

    Changing where the input is detected for pushing blocks

---
+++ Assets/Scripts/Objects/PlayerPush.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Movement;
+
+public class PlayerPush : MonoBehaviour
+{
+    [HideInInspector]
+    public bool pushing;
+
+    PlayerController playerController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region InputCalls
+    
+    public void InputActionGrab(InputAction.CallbackContext action)
+    {
+        pushing = action.ReadValueAsButton();
+    }
+
+    #endregion
+}
