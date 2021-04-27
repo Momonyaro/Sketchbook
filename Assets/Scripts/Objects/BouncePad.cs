de1c471c2e2566f84e7bdc2b411d80843e4c315a
Albin <a19albda@student.his.se>
2021-04-27 11:05:03 +0200

    bounce pad scriiipt

---
+++ Assets/Scripts/Objects/BouncePad.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Movement;
+
+public class BouncePad : MonoBehaviour
+{
+    [Tooltip("The force that will launch the player upwards after bouncing on the enemy")]
+    public float playerBounceDistance = 170.0f;
+
+    PlayerController playerController;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player" && other.gameObject.GetComponent<PlayerController>() != null)
+        {
+            playerController = other.gameObject.GetComponent<PlayerController>();
+            if (!playerController.IsGrounded())
+            {
+                playerController.BounceUsingForce(playerBounceDistance);
+            }
+        }
+    }
+}
