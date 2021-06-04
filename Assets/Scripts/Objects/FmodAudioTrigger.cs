cc81ed1a8bc94dee14b4d2590d1c0b840b514846
Momonyaro <sebastian.alkstrand@gmail.com>
2021-06-04 14:36:32 +0200

    added audio param trigger shizzzzzzzzzzzzzz :100:
    yo mann this be litttt, az hjflgljakm lnkjaäsöld öjöaöaös sus sus sus sus sus sus. Brain sharteddddddzasdzsxd

---
+++ Assets/Scripts/Objects/FmodAudioTrigger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FMODUnity;
+using UnityEngine;
+
+namespace Objects
+{
+    public class FmodAudioTrigger : MonoBehaviour
+    {
+        public StudioEventEmitter emitter;
+        
+        [Header("On Trigger Enter:")]
+        
+        public bool setOnTriggerEnter = true;
+        public string triggerEnterParamTitle = "";
+        public float triggerEnterValue = 0.0f;
+        
+        [Header("On Trigger Exit:")]
+        
+        public bool setyOnTriggerExit = true;
+        public string triggerExitParamTitle = "";
+        public float triggerExitValue = 0.0f;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (!setOnTriggerEnter) return;
+            if (other.CompareTag("Player"))
+            {
+                emitter.SetParameter(triggerEnterParamTitle, triggerEnterValue);
+            }
+        }
+        private void OnTriggerExit(Collider other)
+        {
+            if (!setyOnTriggerExit) return;
+            if (other.CompareTag("Player"))
+            {
+                emitter.SetParameter(triggerExitParamTitle, triggerExitValue);
+            }
+        }
+    }
+}
